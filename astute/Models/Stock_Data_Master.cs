68638ca9cc8a98635b5bf1a4d1fab195dd073da3
DESKTOP\Desktop <maheshvakodikar1995@gmail.com>
2023-10-28 12:40:08 +0530

    added stock data master class

---
+++ astute/Models/Stock_Data_Master.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace astute.Models
+{
+    public partial class Stock_Data_Master
+    {
+        [Key]
+        public int Stock_Data_Id { get; set; }
+        public int? Supplier_Id { get; set; }
+        public string? Upload_Method { get; set; }
+    }
+}
