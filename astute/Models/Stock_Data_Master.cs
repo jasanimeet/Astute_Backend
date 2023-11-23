be42c041942b911b3392dba11769bc43d8fdbc2d
Rinkal <kajal@brainwaves.co.in>
2023-11-23 10:11:49 +0530

    Did Added Upload_Type field on stock_data_master table.

--- astute/Models/Stock_Data_Master.cs
+++ astute/Models/Stock_Data_Master.cs
@@ -10,6 +10,7 @@ namespace astute.Models
         public int Stock_Data_Id { get; set; }
         public int? Supplier_Id { get; set; }
         public string? Upload_Method { get; set; }
+        public string? Upload_Type { get; set; }
         [NotMapped]
         public IList<Stock_Data> Stock_Data_List { get; set; } = new List<Stock_Data>();
     }
