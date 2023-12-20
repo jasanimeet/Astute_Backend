a4fdd562f05005b1090cc1211402f305af077b86
Rinkal <kajal@brainwaves.co.in>
2023-12-20 10:52:46 +0530

    update

---
+++ astute/Models/Report_Detail.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace astute.Models
+{
+    public class Report_Detail
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Rm_Id { get; set; }
+        public string Column_Type { get; set; }
+        public int Col_Id { get; set; }
+        public int Order_No { get; set; }
+        public string Display_Type { get; set; }
+        public int Width { get; set; }
+        public string Column_Format { get; set; }
+        public string Alignment { get; set; }
+        public string Fore_Colour { get; set; }
+        public string Back_Colour { get; set; }
+
+    }
+}
