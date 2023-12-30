e67956f2feb6dec7366f42b93dfe3710ab25b7f1
Rinkal <kajal@brainwaves.co.in>
2023-12-30 13:25:54 +0530

    update

--- astute/Models/Party_API_With_Column_Mapping.cs
+++ astute/Models/Party_API_With_Column_Mapping.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,7 +9,8 @@ namespace astute.Models
     public class Party_API_With_Column_Mapping
     {
         [Key]
-        public int Id { get; set; }
+        public int? API_Id { get; set; }
+        public int? FTP_Id { get; set; }
         public int? Party_Id { get; set; }
         public string? Upload_Type { get; set; }
         public string? API_URL { get; set; }
