ef68f08861f24a0add128d517d26b91b875b1bb8
Rinkal <kajal@brainwaves.co.in>
2024-02-05 16:19:54 +0530

    update

--- astute/Repository/SupplierService.cs
+++ astute/Repository/SupplierService.cs
@@ -1522,7 +1522,7 @@ namespace astute.Repository
                         command.CommandType = CommandType.StoredProcedure;
                         foreach (var item in report_Filter_Parameters.Where(x=>!string.IsNullOrEmpty(x.Category_Value)).ToList())
                         {
-                            command.Parameters.Add(!string.IsNullOrEmpty(item.Category_Value) ? new SqlParameter("@" + item.Column_Name, item.Category_Value) : new SqlParameter("@" + item.Column_Name, DBNull.Value));
+                            command.Parameters.Add(!string.IsNullOrEmpty(item.Category_Value) ? new SqlParameter("@" + item.Column_Name.Replace(" ","_"), item.Category_Value) : new SqlParameter("@" + item.Column_Name.Replace(" ", "_"), DBNull.Value));
                         }
                         command.CommandTimeout = 1800;
                         await connection.OpenAsync();
