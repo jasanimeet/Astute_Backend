0a8bca53ef91152b36abbfda9b9753de0925859a
Rinkal <kajal@brainwaves.co.in>
2024-03-30 17:49:20 +0530

    update

--- astute/Models/Customer_Column_Caption.cs
+++ astute/Models/Customer_Column_Caption.cs
@@ -1,8 +1,12 @@
-﻿namespace astute.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace astute.Models
 {
     public class Customer_Column_Caption
     {
         public int Col_Id { get; set; }
+        [NotMapped]
+        public string Display_Name { get; set; }
         public string Caption_Name { get; set; }
         public bool Status { get; set; }
         public string? Upload_Method { get; set; }
