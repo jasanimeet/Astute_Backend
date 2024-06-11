b6f06d3d618ee56d4aa3cb241a37b5d07465c947
Lenovo <135941841+jasanimeet@users.noreply.github.com>
2024-06-11 17:05:50 +0530

    Added code for Final_Order_Processing_Create_Update for formfile to update the Offer_Amt, Offer_Disc, Order_Cost_Amt, Order_Cost_Disc

--- astute/Repository/SupplierService.cs
+++ astute/Repository/SupplierService.cs
@@ -2974,6 +2974,36 @@ namespace astute.Repository
 
             return result;
         }
+
+        public async Task<int> Final_Order_Processing_Create_Update(DataTable dataTable, int? user_Id)
+        {
+            int totalUpdatedRecords = 0;
+            using (var connection = new SqlConnection(_configuration["ConnectionStrings:AstuteConnection"]))
+            {
+                using (var command = new SqlCommand("Final_Order_Processing_Create_Update", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Final_Order_Processing_Type", dataTable);
+                    command.Parameters.AddWithValue("@User_Id", user_Id);
+
+                    var totalUpdatedRecordsParameter = new SqlParameter("@TotalUpdatedRecords", SqlDbType.Int)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+                    command.Parameters.Add(totalUpdatedRecordsParameter);
+
+                    command.CommandTimeout = 1800;
+                    await connection.OpenAsync();
+
+                    await command.ExecuteNonQueryAsync();
+
+                    totalUpdatedRecords = (int)totalUpdatedRecordsParameter.Value;
+                }
+            }
+
+            return totalUpdatedRecords;
+        }
+
         #endregion
     }
 }
