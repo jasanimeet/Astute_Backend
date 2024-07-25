b6dff0b35cff93b538f81607cfcb549724149a54
Lenovo <135941841+jasanimeet@users.noreply.github.com>
2024-07-25 16:15:58 +0530

    update in expense

--- astute/Models/Expense_Trans_Det.cs
+++ astute/Models/Expense_Trans_Det.cs
@@ -5,11 +5,11 @@ namespace astute.Models
     {
         public int Expense_Trans_Det_Id { get; set; }
         public int Account_Master_Id { get; set; }
-        public string Sign { get; set; }
+        public string? Sign { get; set; }
         [JsonProperty("per")]
-        public decimal Percentage { get; set; }
-        public decimal amount { get; set; }
+        public decimal? Percentage { get; set; }
+        public decimal? amount { get; set; }
         [JsonProperty("amount $")]
-        public decimal amount_Dollar { get; set; }
+        public decimal? amount_Dollar { get; set; }
     }
 }
