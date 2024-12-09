87d945b81a3dd38a8338e62342191a2ae05f30ea
swet <maheshvakodikar1995@gmail.com>
2024-12-09 10:41:08 +0530

    implemented DIANCO (HK) LTD - S Api

---
+++ astute/Models/Dianco_Model.cs
@@ -0,0 +1,14 @@
+﻿namespace astute.Models
+{
+    public class Dianco_Model
+    {
+        public string Login_URL { get; set; }
+        public string User_Caption { get; set; }
+        public string User_Name { get; set; }
+        public string Password_Caption { get; set; }
+        public string Password { get; set; }
+        public string Action_Caption { get; set; }
+        public string Action_Value { get; set; }
+        public string Stock_Url { get; set; }
+    }
+}
