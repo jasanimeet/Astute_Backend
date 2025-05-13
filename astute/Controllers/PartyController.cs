c60747d9aba3c60e8c2da7de11ea0c71a9bc3311
rinkalparmar63999@gmail.com <rinkalparmar63999@gmail.com>
2025-05-13 15:04:47 +0530

    Diarough Api

--- astute/Controllers/PartyController.cs
+++ astute/Controllers/PartyController.cs
@@ -19701,6 +19701,106 @@ namespace astute.Controllers
 
         #endregion
 
+        #region Diarough Api
+        /*
+         * Added By Jashmin Patel(2025/05/13)
+         * For Diarough API.
+         */
+        [HttpPost]
+        [Route("get_diarough_stock")]
+        public async Task<IActionResult> Get_Diarough_Stock(Diarough_Model model)
+        {
+            try
+            {
+                var _client = new HttpClient();
+
+                var _request = new HttpRequestMessage(HttpMethod.Post, model.Login_URL);
+
+                var _credentials = new Dictionary<string, string>
+                {
+                    { model.User_Caption , model.User_Name},
+                    { model.Password_Caption, model.Password }
+                };
+
+                var _content = new StringContent(JsonConvert.SerializeObject(_credentials), Encoding.UTF8, "application/json");
+
+                _request.Content = _content;
+
+                var _response = await _client.SendAsync(_request);
+
+                if (_response.IsSuccessStatusCode)
+                {
+                    var json = await _response.Content.ReadAsStringAsync();
+                    string cleanedJson = json.Replace("\\\"", "\"").Trim('"');
+
+                    var jsonObject = JsonConvert.DeserializeObject<JObject>(cleanedJson);
+
+                    var partyId = jsonObject["Data"]?["Party_Id"]?.ToString();
+                    var token = jsonObject["Data"]?["Token"]?.ToString();
+
+                    using (var client = new HttpClient())
+                    {
+                        var stockUrl = $"{model.Stock_Url}?{model.Action_Caption}={partyId}&{model.Action_Caption1}={token}";
+                        var request = new HttpRequestMessage(HttpMethod.Get, stockUrl);
+                        var response = await client.SendAsync(request);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var responseString = await response.Content.ReadAsStringAsync();
+
+                            return StatusCode((int)response.StatusCode, responseString);
+                        }
+                        else
+                        {
+                            var errorDetails = await response.Content.ReadAsStringAsync();
+
+                            await _commonService.InsertErrorLog(CoreCommonMessage.ApiFailed, "Get_Diarough_Stock", errorDetails);
+                            return Conflict(new
+                            {
+                                statusCode = HttpStatusCode.Conflict,
+                                message = CoreCommonMessage.ApiFailed,
+                                error = errorDetails
+                            });
+                        }
+                    }
+                }
+                else
+                {
+                    var errorDetails = await _response.Content.ReadAsStringAsync();
+
+                    await _commonService.InsertErrorLog(CoreCommonMessage.ApiFailed, "Get_Diarough_Stock", errorDetails);
+                    return Conflict(new
+                    {
+                        statusCode = HttpStatusCode.Conflict,
+                        message = CoreCommonMessage.ApiFailed,
+                        error = errorDetails
+                    });
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                await _commonService.InsertErrorLog(httpEx.Message, "Get_Diarough_Stock", httpEx.StackTrace);
+                return StatusCode((int)HttpStatusCode.InternalServerError, new
+                {
+                    statusCode = HttpStatusCode.InternalServerError,
+                    message = CoreCommonMessage.ApiError,
+                    error = httpEx.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                await _commonService.InsertErrorLog(ex.Message, "Get_Diarough_Stock", ex.StackTrace);
+                return StatusCode((int)HttpStatusCode.InternalServerError, new
+                {
+                    statusCode = HttpStatusCode.InternalServerError,
+                    message = ex.Message,
+                    error = ex.StackTrace
+                });
+            }
+        }
+
+        #endregion
+
         #region Supplier Special Price Validity
         [HttpPost]
         [Route("get_supplier_special_price_validity")]
